e979e11da97d981156bbe2d21f802edc63af161b
andreasaasen <andreasaasen37@gmail.com>
2019-03-16 23:40:38 +0100

    dsdas

--- index.js
+++
@@ -1,9 +0,0 @@
-function openMenu(){
-    document.getElementById('side-menu').style.width= '200px';
-
-}
-
-function closeMenu(){
-    document.getElementById('side-menu').style.width= '0';
-    
-}
